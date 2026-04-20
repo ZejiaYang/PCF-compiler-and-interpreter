73722fb2108a04f2e999a4cd8e5b63d268eac218
zejia yang <zejiayang2003@gmail.com>
2026-04-20 06:50:41 +0100

    complete call-by-value interpreter

--- lib/interp.ml
+++ lib/interp.ml
@@ -1,15 +1,16 @@
 open Term
 
-type env = END | NEXT of string * thunk * env
-and thunk = term * env
+type env = END | NEXT of string * closure * env
+and closure = term * env
 
-type closure = term * env
+type interpreter = closure -> closure
 
 let rec find (x : string) = function
   | END -> failwith "cannot find x in env"
   | NEXT (y, t, e) -> if x == y then t else find x e
 
-let rec interp_by_name ((p, e) : closure) : closure =
+let rec interp_by_name : interpreter =
+ fun (p, e) ->
   match p with
   | VAR x ->
       let t, e = find x e in
@@ -38,3 +39,40 @@ let rec interp_by_name ((p, e) : closure) : closure =
       | _ -> failwith "not a function in application")
   | LET (x, p1, p2) -> interp_by_name (APP (FUN (x, p2), p1), e)
   | FIX (x, p1) -> interp_by_name (APP (FUN (x, p1), p), e)
+
+(* for fixed point operator, cannot interp by value *)
+(* extended values *)
+let rec interp_by_value : interpreter =
+ fun (p, e) ->
+  match p with
+  | VAR x -> (
+      let y, e = find x e in
+      match y with
+      | INT _ | FUN _ -> (y, e) (* values *)
+      | FIX _ -> interp_by_value (y, e) (* extended values*)
+      | _ -> failwith "env not storing values")
+  | INT _ -> (p, END) (* do not store extra env*)
+  | FUN _ -> (p, e)
+  | BOP (p1, op, p2) -> (
+      let v1, _ = interp_by_value (p1, e) in
+      let v2, _ = interp_by_value (p2, e) in
+      let e = END in
+      match (v1, op, v2) with
+      | INT n1, ADD, INT n2 -> (INT (n1 + n2), e)
+      | INT n1, MINUS, INT n2 -> (INT (n1 - n2), e)
+      | INT n1, MULTI, INT n2 -> (INT (n1 * n2), e)
+      | INT n1, DIVIDE, INT 0 -> failwith "divide by zero"
+      | INT n1, DIVIDE, INT n2 -> (INT (n1 / n2), e)
+      | _ -> failwith "binary operands not integer")
+  | IFZ (p1, p2, p3) -> (
+      match interp_by_value (p1, e) with
+      | INT n, _ ->
+          if n == 0 then interp_by_value (p2, e) else interp_by_value (p3, e)
+      | _ -> failwith "if condition not bool")
+  | APP (p1, p2) -> (
+      match interp_by_value (p1, e) with
+      | FUN (x, t), e1 ->
+          interp_by_value (t, NEXT (x, interp_by_value (p2, e), e1))
+      | _ -> failwith "not a function in application")
+  | LET (x, p1, p2) -> interp_by_value (APP (FUN (x, p2), p1), e)
+  | FIX (x, p1) -> interp_by_value (p1, NEXT (x, (p, e), e))
--- test/test_interp.ml
+++ test/test_interp.ml
@@ -5,20 +5,19 @@ open Alcotest
 
 let value = testable pp_term ( = )
 
-let test_interp_shadowing () =
-  (* (fun x -> fun x -> x) 2 3 => 3 *)
+let test_interp_shadowing (interp : interpreter) () =
   let t = APP (APP (FUN ("x", FUN ("x", VAR "x")), INT 2), INT 3) in
-  check value "shadowing" (INT 3) (fst (interp_by_name (t, END)))
+  check value "shadowing" (INT 3) (fst (interp (t, END)))
 
-let test_interp_higher_order () =
+let test_interp_higher_order (interp : interpreter) () =
   (* (fun x -> fun y -> (fun x -> x + y) x) 4 5 ==> 9 *)
   let inner = FUN ("x", VAR "x" ++ VAR "y") in
   let t =
     APP (APP (FUN ("x", FUN ("y", APP (inner, VAR "x"))), INT 4), INT 5)
   in
-  check value "higher order" (INT 9) (fst (interp_by_name (t, END)))
+  check value "higher order" (INT 9) (fst (interp (t, END)))
 
-let test_interp_static_vs_dynamic () =
+let test_interp_static_vs_dynamic (interp : interpreter) () =
   (*
     let x = 4 in
     let f = fun y -> y + x in
@@ -30,21 +29,33 @@ let test_interp_static_vs_dynamic () =
   let t =
     LET ("x", INT 4, LET ("f", f_body, LET ("x", INT 5, APP (VAR "f", INT 6))))
   in
-  check value "static binding" (INT 10) (fst (interp_by_name (t, END)))
+  check value "static binding" (INT 10) (fst (interp (t, END)))
 
-let test_interp_call_by_name () =
+let test_interp_call_by_name (interp : interpreter) () =
   (* (fun x -> 0) ((fix f. fun x -> f x) 0) ==> 0 *)
   let diverge = APP (FIX ("f", FUN ("x", APP (VAR "f", VAR "x"))), INT 0) in
   let t = APP (FUN ("x", INT 0), diverge) in
-  check value "call by name ignores argument" (INT 0)
-    (fst (interp_by_name (t, END)))
+  check value "call by name ignores argument" (INT 0) (fst (interp (t, END)))
 
-let test_interp_suite =
+let test_fact (interp : interpreter) () =
+  let fact =
+    FIX
+      ( "f",
+        FUN
+          ("x", IFZ (VAR "x", INT 1, VAR "x" ** APP (VAR "f", VAR "x" -- INT 1)))
+      )
+  in
+  let t = APP (fact, INT 3) in
+  check value "factorial" (INT 6) (fst (interp (t, END)))
+
+let make_interp_tests (interp : interpreter) =
   [
-    ("shadowing", `Quick, test_interp_shadowing);
-    ("higher_order", `Quick, test_interp_higher_order);
-    ("static_binding", `Quick, test_interp_static_vs_dynamic);
-    ("call_by_name", `Quick, test_interp_call_by_name);
+    ("shadowing", `Quick, test_interp_shadowing interp);
+    ("higher_order", `Quick, test_interp_higher_order interp);
+    ("static_binding", `Quick, test_interp_static_vs_dynamic interp);
+    ("fact", `Quick, test_fact interp);
   ]
 
-let () = run "interp_by_name suite" [ ("pcf tests", test_interp_suite) ]
+let () =
+  run "interp_by_value suite"
+    [ ("pcf tests", make_interp_tests interp_by_value) ]
